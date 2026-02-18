20697aadcf2000df9c80d219dd01bb143fd65ca7
kamaleshpramanik <44993061+kamaleshpramanik@users.noreply.github.com>
2026-02-19 01:06:01 +0530

    Update API base URL for todo API

--- frontend/src/api/todoApi.js
+++ frontend/src/api/todoApi.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api/todos";
+//const API_BASE_URL = "http://localhost:8080/api/todos";
+//const API_BASE_URL = "http://13.201.205.120:8080/api/todos";
+const API_BASE_URL = "https://kamstar-test.shop/api/todos";
+//const API_BASE_URL = "https://todo-backend-alb-699487798.ap-south-1.elb.amazonaws.com/api/todos";
 
 export const getTodos = () => axios.get(API_BASE_URL);
 
@@ -12,3 +15,4 @@ export const updateTodo = (id, todo) =>
 
 export const deleteTodo = (id) =>
   axios.delete(`${API_BASE_URL}/${id}`);
+
